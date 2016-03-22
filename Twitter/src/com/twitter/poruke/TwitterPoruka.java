0f7efa45f885f052b33b39fe6d6a8d7492157d18
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2016-03-22 20:59:45 +0100

    Napisana dokumentacija za klasu TwitterPoruka za sve atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,14 +1,30 @@
 package com.twitter.poruke;
 
 public class TwitterPoruka {
+	
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+	
+	/**
+	 * Poruka korisnika
+	 */
 	private String poruka;
 	
-	
+	/**
+	 * Vraca vrednost atributa korisnik
+	 * @return ime korisnika
+	 */
 	public String getKorisnik() {
 	return korisnik;
 	}
 	
+	/**
+	 * Postavlja se nova vrednost atributa korisnik
+	 * @param korisnik
+	 * @throws java.lang.RuntimeException ako ime nije uneto.
+	 */
 	public void setKorisnik(String korisnik) {
 	if (korisnik==null || korisnik == "")
 	throw new RuntimeException(
@@ -16,10 +32,20 @@ public class TwitterPoruka {
 	this.korisnik = korisnik;
 	}
 	
-	
+	/**
+	 * Vraca vrednost atributa poruka.
+	 * @return poruka korisnika
+	 */
 	public String getPoruka() {
 	return "poruka";
 	}
+	
+	/**
+	 * Postavlja se nova vrednost atributa
+	 * @param poruka
+	 * @throws java.lang.RuntimeException ako poruka nije uneta
+	 * ili ako je poruka duza od 140 karaktera.
+	 */
 	public void setPoruka(String poruka) {
 	if (poruka==null || this.poruka.length()>140)
 	throw new RuntimeException(
@@ -27,7 +53,9 @@ public class TwitterPoruka {
 	this.poruka = poruka;
 	}
 	
-	
+	/**
+	 * Redefinisana toString() metoda za klasu TwitterPoruka
+	 */
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
